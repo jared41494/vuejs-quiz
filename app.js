575f3e3844e86cef8d96cb38e3e41a4b205cb4de
Jared Rummell <jared@rummell.net>
2019-06-02 12:23:34 -0400

    app.js revision from static folder to dist folder

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ let app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
 app.get('/', (req, res) => {
-	res.sendFile(path.join(__dirname, 'static', 'index.html'));
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.listen(process.env.PORT || 5000);
