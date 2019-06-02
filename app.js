767011ebc837b496a7fb55a5b758bace7d2cb1f8
Jared Rummell <jared@rummell.net>
2019-06-02 14:47:27 -0400

    Still revising app.js

--- app.js
+++ app.js
@@ -5,12 +5,12 @@ var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
 
-app.get('/public', (req, res) => {
+/*app.get('/public', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+});*/
 
 app.listen(process.env.PORT || 5000);
