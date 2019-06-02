948a0fcadb523d31a965937d8148e3f3436234a7
Jared Rummell <jared@rummell.net>
2019-06-02 12:32:18 -0400

    another edit to app.js for routes

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ var express = require('express');
 var path = require('path');
 var serveStatic = require('serve-static');
 
-let app = express();
+app = express();
 app.use(serveStatic(__dirname + "/dist"));
+
+app.get('/public', (req, res) => {
+	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 app.get('/', (req, res) => {
 	res.sendFile(path.join(__dirname, 'dist', 'index.html'));
