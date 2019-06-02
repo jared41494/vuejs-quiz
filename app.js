9fdb0f06db4ab28cc6580b1e2c85961bd7f84cfb
Jared Rummell <jared@rummell.net>
2019-06-02 11:20:57 -0400

    revise app.js to run application

--- app.js
+++ app.js
@@ -1,8 +1,7 @@
 const express = require('express');
-const serveStatic = require("serve-static");
 const path = require('path');
 
-let app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
-const port = process.env.PORT || 80;
-app.listen(port);
+let app = express()
+app.use('/public', express.static(path.join(__dirname, 'public')));
+
+app.listen(process.env.PORT || 80);
