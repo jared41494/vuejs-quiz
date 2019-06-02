44c9088c1a94dc0b7f681c48f9bf2827137c383e
Jared Rummell <jared@rummell.net>
2019-06-02 11:24:41 -0400

    another small edit to app.js to get it to run

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ const path = require('path');
 let app = express()
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
-app.listen(process.env.PORT || 80);
+app.listen(process.env.PORT || 3000);
