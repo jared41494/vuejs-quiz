59ad56e182222069ae0990d16968319a123159ff
Jared Rummell <jared@rummell.net>
2019-06-02 12:20:01 -0400

    app.js needs to know which file to serve up: index.html

--- app.js
+++ app.js
@@ -4,4 +4,9 @@ var serveStatic = require('serve-static');
 
 let app = express();
 app.use(serveStatic(__dirname + "/dist"));
+
+app.get('/', (req, res) => {
+	res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
+
 app.listen(process.env.PORT || 5000);
